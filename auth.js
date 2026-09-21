7a533e4f45ce40eee265aad5dd64910f3210ca26
THANHTRUONG06082006-VN <thanh.2474802010361@vanlanguni.vn>
2026-09-21 14:05:56 +0700

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,8 @@
+function login(username, password) {
+  if (!username || !password) return false;
+  if (username === 'locked_user') return false;
+  if (username === 'admin' && password === '123') return true;
+  return false;
+}
+
+module.exports = { login };
